96b481be1be712ef495869224101492649e62380
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2015-03-05 19:04:47 +0000

    Small tide up of code

--- bin/monitor_running_polymarker.rb
+++ bin/monitor_running_polymarker.rb
@@ -25,13 +25,10 @@ pol=Bio::DB::Polymarker.new(options[:preferences])
 
 #pol.mysql_version
 pol.each_running do |row|
-  #puts row.join(",")
-  #puts row.inspect
   pol.review_running_status(row[0], row[1])  
 end
 
 pol.each_timeout do |row|
-  # => puts "Timeouts: #{row[0]}"
   pol.update_error_status(row[0], "Timeout")
 end
 
